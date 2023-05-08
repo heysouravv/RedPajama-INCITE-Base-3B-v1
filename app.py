176f3d5c810288c10eb68cef795722b0e6b1ff99
Sourav  Padhi <97221197+heysouravv@users.noreply.github.com>
2023-05-08 13:04:15 +0530

    New Push

--- app.py
+++ app.py
@@ -1,49 +1,38 @@
-from potassium import Potassium, Request, Response
-from transformers import pipeline
-from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+from transformers import pipeline , AutoTokenizer, AutoModelForCausalLM
 
-app = Potassium("my_app")
 
-# @app.init runs at startup, and loads models into the app's context
-@app.init
+# Init is ran on server startup
+# Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    device = 0 if torch.cuda.is_available() else -1
+    global model
+    global tokenizer
+
     tokenizer = AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1")
-    model = AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1", torch_dtype=torch.float16)
-    model = model.to('cuda:0')
-    context = {
-        "tokenizer": tokenizer,
-        "model": model
-    }
+    model = AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1",torch_dtype=torch.float16).to("cuda")
 
-    return context
-
-# @app.handler runs for every call
-@app.handler()
-def handler(context: dict, request: Request) -> Response:
-    prompt = request.json.get("prompt")
-    max_new_tokens = request.json.get("max_new_tokens")
-    temperature = request.json.get("temperature")
-    top_k = request.json.get("top_k")
-    top_p = request.json.get("top_p")
-    do_sample = request.json.get("do_sample")
-    model = context.get("model")
-    tokenizer = context.get('tokenizer')
-    inputs = tokenizer(prompt, return_tensors='pt').to(model.device)
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model_inputs:dict) -> dict:
+    global model
+    global tokenizer
+    # Parse out your arguments
+    prompt = model_inputs.get('prompt', None)
+    max_new_tokens = model_inputs.get("max_new_tokens", None)
+    temperature = model_inputs.get("temperature", None)
+    top_k = model_inputs.get("top_k", None)
+    top_p = model_inputs.get("top_p", None)
+    do_sample = model_inputs.get("do_sample", None)
+    if prompt == None or max_new_tokens == None or temperature == None or top_k == None or top_p == None or do_sample == None:
+        return {'message': "Insufficient arguments"}
+    
+    # Run the model
+    inputs = tokenizer(prompt, return_tensors='pt').to("cuda")
     input_length = inputs.input_ids.shape[1]
     outputs = model.generate(
         **inputs, max_new_tokens=max_new_tokens, do_sample=do_sample, temperature=temperature, top_p=top_p, top_k=top_k, return_dict_in_generate=True,
     )
     token = outputs.sequences[0, input_length:]
     output_str = tokenizer.decode(token)
-    print(output_str)
-    outputs = model(prompt)
-
-    return Response(
-        json = {"outputs": outputs}, 
-        status=200
-    )
-
-if __name__ == "__main__":
-    app.serve()
+    # Return the results as a dictionary
+    return output_str
--- download.py
+++ download.py
@@ -2,13 +2,12 @@
 
 # In this example: A Huggingface BERT model
 from transformers import pipeline , AutoTokenizer, AutoModelForCausalLM
-import torch
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     # pipeline('fill-mask', model='bert-base-uncased')
     AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1")
-    AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1", torch_dtype=torch.float16)
+    AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1")
 
 if __name__ == "__main__":
     download_model()
