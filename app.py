22254d19eeef30eca61b329a1ac10ac8a29020fb
Sourav  Padhi <97221197+heysouravv@users.noreply.github.com>
2023-05-08 09:12:36 +0530

    First Push

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from potassium import Potassium, Request, Response
-
 from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 app = Potassium("my_app")
@@ -9,9 +9,11 @@ app = Potassium("my_app")
 @app.init
 def init():
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-   
+    tokenizer = AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1")
+    model = AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1", torch_dtype=torch.float16)
+    model = model.to('cuda:0')
     context = {
+        "tokenizer": tokenizer,
         "model": model
     }
 
@@ -21,11 +23,25 @@ def init():
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
     prompt = request.json.get("prompt")
+    max_new_tokens = request.json.get("max_new_tokens")
+    temperature = request.json.get("temperature")
+    top_k = request.json.get("top_k")
+    top_p = request.json.get("top_p")
+    do_sample = request.json.get("do_sample")
     model = context.get("model")
+    tokenizer = context.get('tokenizer')
+    inputs = tokenizer(prompt, return_tensors='pt').to(model.device)
+    input_length = inputs.input_ids.shape[1]
+    outputs = model.generate(
+        **inputs, max_new_tokens=max_new_tokens, do_sample=do_sample, temperature=temperature, top_p=top_p, top_k=top_k, return_dict_in_generate=True,
+    )
+    token = outputs.sequences[0, input_length:]
+    output_str = tokenizer.decode(token)
+    print(output_str)
     outputs = model(prompt)
 
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json = {"outputs": outputs}, 
         status=200
     )
 
--- download.py
+++ download.py
@@ -1,11 +1,14 @@
 # This file runs during container build time to get model weights built into the container
 
 # In this example: A Huggingface BERT model
-from transformers import pipeline
+from transformers import pipeline , AutoTokenizer, AutoModelForCausalLM
+import torch
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    # pipeline('fill-mask', model='bert-base-uncased')
+    AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1")
+    AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Base-3B-v1", torch_dtype=torch.float16)
 
 if __name__ == "__main__":
     download_model()
